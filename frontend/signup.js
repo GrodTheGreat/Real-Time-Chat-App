c5dd98549f0ab5276858b83a65d441cec85a72a7
GrodTheGreat <60365291+GrodTheGreat@users.noreply.github.com>
2025-02-17 20:56:11 -0500

    Add files via upload

--- frontend/signup.js
+++ frontend/signup.js
@@ -1,40 +1,53 @@
-async function signUp(event) {
-  event.preventDefault();
-  window.onbeforeunload = () => "";
+(() => {
+  async function signUp(event) {
+    event.preventDefault();
+    window.onbeforeunload = () => "";
 
-  const username = signUpForm.querySelector("#username").value;
-  const firstName = signUpForm.querySelector("#first").value;
-  const lastName = signUpForm.querySelector("#last").value;
-  const password = signUpForm.querySelector("#password").value;
-  const confirmedPassword = signUpForm.querySelector("#confirm").value;
+    const username = signUpForm.querySelector("#username").value;
+    const firstName = signUpForm.querySelector("#first").value;
+    const lastName = signUpForm.querySelector("#last").value;
+    const password = signUpForm.querySelector("#password").value;
+    const confirmedPassword = signUpForm.querySelector("#confirm").value;
 
-  if (!username) {
-    alert("You must enter a username!");
-    throw Error("You must enter a password");
+    if (!username) {
+      alert("You must enter a username!");
+      console.error("You must enter a password");
+      return;
+    }
+    if (password !== confirmedPassword) {
+      alert("Passwords must match!");
+      console.error("Passwords must match!");
+      return;
+    }
+    if (password.length < 12) {
+      alert("Password must be longer");
+      console.log("Password must be longer");
+      return;
+    } else if (password.length < 16) {
+      alert("Consider using a longer password");
+      console.warn("Consider using a longer password");
+    }
+
+    const response = await fetch("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        firstName,
+        lastName,
+      }),
+    });
+    if (!response.ok) {
+      console.error("Failed to sign up!");
+      return;
+    }
+    //   What should I do with this?
+    const result = await response.json();
   }
-  if (password != confirmedPassword) {
-    alert("Passwords must match!");
-    throw Error("Passwords must match!");
-  }
 
-  const response = await fetch("/signup", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      username,
-      password,
-      firstName,
-      lastName,
-    }),
-  });
-  if (!response.ok) {
-    throw Error("Failed to sign up!");
-  }
-  //   What should I do with this?
-  const result = await response.json();
-}
-
-const signUpForm = document.getElementById("signup-form");
-signUpForm.addEventListener("submit", signUp);
+  const signUpForm = document.getElementById("signup-form");
+  signUpForm.addEventListener("submit", signUp);
+})();
